f26ef42c2dae53cf5a2e4bd9108248897f48e5e2
eal6gf <elum1234@gmail.com>
2023-04-29 16:09:44 -0400

    add some closure practice examples

--- index.js
+++ index.js
@@ -1,9 +1,56 @@
+function getById(name) {
+  return document.getElementById(name);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("percent").addEventListener("change", validate);
+  getById("percent").addEventListener("change", validate);
   function validate(e) {
+    console.log(this);
     var value = e.target.value;
     if (value > 100 || value < 0) {
       alert("you can't do that");
     }
   }
 });
+
+function log (arg){
+    console.log(arg)
+}
+
+// IIFE way for private variables
+var bicycle = (function(){
+    var intWheels = 0
+    function setWheels(arg){
+        intWheels = arg
+    }
+
+    return {
+        getWheels(){
+            return intWheels
+        },
+        modify(arg){
+            setWheels(arg)
+        }
+    }
+})()
+
+
+// Factory way for making Bicycles
+const makeBike = function(){
+    var wheels = 0;
+    function setWheels(arg){
+        wheels = arg
+    }
+
+    return {
+        getWheels(){
+            return wheels
+        },
+        modWheels(arg){
+            setWheels(arg)
+        }
+    }
+}
+
+const secondBike = makeBike()
+const thirdBike = makeBike()
